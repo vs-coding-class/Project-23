5d89fb4e6a38b29e41cd803d7aaf5561fdca0c54
vs-coding-class <65456951+vs-coding-class@users.noreply.github.com>
2020-06-14 09:02:04 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,21 +1,23 @@
 var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground;
-var sideRect1,sideRect2,bottomRect;
-var world, engine;
+var packageBody,ground
+var boxY, boxPosition;
+var boxLeftSprite,boxRightSprite,boxLeftBody,boxRightBody,boxBase,boxBottomBody;
+var engine, world;
 
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-const Render = Matter.Render;
 
 function preload(){
-	helicopterIMG=loadImage("helicopter.png");
-	packageIMG=loadImage("package.png");
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
 }
 
-function setup(){
+function setup() {
 	createCanvas(800, 700);
+	rectMode(CENTER);
+	
 
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG);
@@ -28,39 +30,47 @@ function setup(){
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255);
 
-	engine = Engine.create();
-	world = engine.World;
 
-	packageBody = Bodies.circle(width/2,200,5,{isStatic:true});
-	World.add(world,packageBody);
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:-1, isStatic:true});
+	World.add(world, packageBody);
 
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
-	World.add(world,ground);
+ 	World.add(world, ground);
 
-	sideRect1 = new landingZone(width/2 - 110,650,20,100);
-	sideRect2 = new landingZone(width/2 + 110,650,20,100);
-	bottomRect = new landingZone(width/2,690,200,20);
+ 	boxPosition=width/2-100
+ 	boxY=640;
+
+ 	boxLeftBody = new landingZone(boxPosition+20,boxY,20,100);
+
+ 	boxBottomBody = new landingZone(boxPosition + 100,boxY,200,20);
+
+ 	boxRightBody = new landingZone(boxPosition + 180,boxY,20,100);
+
 
 	Engine.run(engine);
+  
 }
 
 
 function draw() {
   rectMode(CENTER);
   background(0);
-
+ 
   packageSprite.x= packageBody.position.x;
   packageSprite.y= packageBody.position.y;
 
-  sideRect1.display();
-  sideRect2.display();
-  bottomRect.display();
+  boxLeftBody.display();
+  boxRightBody.display();
+  boxBottomBody.display();
 
   drawSprites();
 }
 
 function keyPressed() {
- 	if (keyCode === DOWN_ARROW) {
+	if (keyCode === DOWN_ARROW) {
     	Matter.Body.setStatic(packageBody,false);
-  	}
+    }
 }
